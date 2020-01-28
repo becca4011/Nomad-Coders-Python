eda8372b56d2c7afa312dbfee43cf7ff6a593672
becca4011 <becca4011@naver.com>
2020-01-28 10:08:30 +0000

    practice

---
+++ main.py
@@ -0,0 +1,11 @@
+a_string = "Hello Python!";
+a_number = 2;
+a_float = 3.14;
+a_boolean = True;
+a_none = None;
+
+print(type(a_string));
+print(type(a_number));
+print(type(a_float));
+print(type(a_boolean));
+print(type(a_none));
